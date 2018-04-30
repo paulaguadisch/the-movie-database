37b55351991096844d07531d59257c4ad2a2983e
Paul A <paul.aguadisch@gmail.com>
2018-04-30 12:53:07 +0200

    Ajout sur heroku

---
+++ db/migrate/20180430100306_create_movies.rb
@@ -0,0 +1,9 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+    	t.string :title
+    	t.integer :release_year
+    	t.string :director
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Movie.destroy_all
+
+Movie.create(title:"Pulp Fiction", release_year:1994, director:"Quentin Tarentino")
+Movie.create(title:"Fight Club", release_year:1999, director:"David Fincher")
+Movie.create(title:"Inception", release_year:2010, director:"Christopher Nolan")
+Movie.create(title:"The Grand Budapest Hotel", release_year:2013, director:"Wes Anderson")
+Movie.create(title:"La Haine", release_year:1995, director:"Mathieu Kassovitz")
