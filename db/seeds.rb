e490ed3a930519627806f55377d1790d9e6d477f
Paul A <paul.aguadisch@gmail.com>
2018-04-30 13:02:30 +0200

    ajout seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-#Movie.destroy_all
-
 Movie.create(title:"Pulp Fiction", release_year:1994, director:"Quentin Tarentino")
 Movie.create(title:"Fight Club", release_year:1999, director:"David Fincher")
 Movie.create(title:"Inception", release_year:2010, director:"Christopher Nolan")
